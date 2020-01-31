1a87f7bf71227f96f60f507dcbf4c08b5241247d
Pankaj Gupta <pankajg@brickredsys.com>
2020-01-31 21:17:56 +0530

    puppeteer added again

---
+++ jest.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    // globalSetup: './src/pup-js/setup.js',
+    // globalTeardown: './src/pup-js/teardown.js',
+    // testEnvironment: './src/pup-js/puppeteer_environment.js',
+  }
