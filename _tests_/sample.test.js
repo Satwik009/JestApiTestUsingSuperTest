a92ae1cc6192a9ee517ca99d82752636c304eb9b
Pankaj Gupta <pankajg@brickredsys.com>
2020-01-31 15:56:45 +0530

    reformatted

--- _tests_/sample.test.js
+++ _tests_/sample.test.js
@@ -1,22 +1,18 @@
-const B = require('../src/samplerequest');
-const C = require('../src/sampleget');
-const D = require('../src/samplepost');
+const B = require("../src/samplerequest");
+const C = require("../src/sampleget");
+const D = require("../src/samplepost");
 
+test("addition of two number", () => {
+  var bc = new B();
+  expect(bc.addition(8, 7)).toBe(15);
+});
 
-test('addition of two number',()=>
-{
-var bc = new B();
-expect(bc.addition(8,7)).toBe(15);
-})
+test("get the list of stores available from GET Request", () => {
+  var ab = new C();
+  ab.getListOfStores();
+});
 
-test('get the list of stores available from GET Request',()=>
-{
-    var ab = new C();
-    ab.getListOfStores();
-})
-
-test('create new store from POST Request',()=>
-{
-    var cd = new D();
-     cd.createNewStores();
-})
+test("create new store from POST Request", () => {
+  var cd = new D();
+  cd.createNewStores();
+});
